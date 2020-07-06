6a9a166ba68d61d6244ed4cbbdb7fb61a93b6f73
Jigar Mehta <jigar.mehta@siemens-healthineers.com>
2020-07-06 11:32:49 +0530

    initial skeleton

---
+++ Source/PromotionEngine.Service.AutoPromotion/Controllers/PromoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PromotionEngine.Service.AutoPromotion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromoController : ControllerBase
+    {
+        public PromoController()
+        {
+
+        }
+
+        [HttpGet]
+        public string GetFinalPrice([FromBody] Dictionary<string, int> cartOrder)
+        {
+            return string.Empty;
+        }
+
+    }
+}
