db427c95779c9fbfde88e195b822c7981a0ca04a
Jigar Mehta <jigar.mehta@siemens-healthineers.com>
2020-07-06 12:18:58 +0530

    initial test skeleton

---
+++ Test/PromotionEngine.Service.AutoPromotion_uTest/Controllers/PromoControllerTest.cs
@@ -0,0 +1,20 @@
+using PromotionEngine.Service.AutoPromotion.Controllers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PromotionEngine.Service.AutoPromotion_uTest.Controllers
+{
+    public class PromoControllerTest
+    {
+        private readonly PromoController myPromoController;
+
+        public PromoControllerTest()
+        {
+            myPromoController = new PromoController();
+        }
+
+        
+
+    }
+}
